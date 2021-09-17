b37111861e667dc57d82b83c1da4666e2bf8102f
Kihyun Nam <skarlgus0910@gmail.com>
2021-09-17 19:50:46 +0900

    Rearrange gradle configuration

--- app/src/main/java/org/techtown/graduateproject/StartActivity.kt
+++ app/src/main/java/org/techtown/graduateproject/StartActivity.kt
@@ -1,9 +1,18 @@
-/*package org.techtown.graduateproject
+package org.techtown.graduateproject
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class StartActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+        val startButton : Button = findViewById(R.id.startButton)
+        startButton.setOnClickListener{
+            
+        }
     }
-}*/
+}
